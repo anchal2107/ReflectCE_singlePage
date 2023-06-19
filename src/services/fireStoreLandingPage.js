99235ca6b7397541355c78df39b5743fda4f3154
Anchal Agarwal <anchal2107@yahoo.com>
2023-06-19 15:29:44 +0530

    adding firestore

---
+++ src/services/fireStoreLandingPage.js
@@ -0,0 +1,77 @@
+import {
+    searchDocuments,
+    createDocument,
+    readAllDocuments,
+    updateDocumentFieldsOnDocId
+  } from "../utils/firebase/firestore";
+
+  async function getPageData() {
+    const resultData = await readAllDocuments(collectionName);
+    console.log('resultData:', resultData);
+    if (resultData) {
+      // Documents found
+      return {
+        code: "SUCCESS",
+        status: 200,
+        message: "Users Found",
+        data: { resultData },
+      };
+    } else {
+      // No documents found
+      return {
+        code: "SUCCESS",
+        status: 200,
+        message: "Users Not Found",
+        data: { resultData: null },
+      };
+    }
+  }
+  
+
+  module.exports = {
+    getPageData
+  };
+  
+  //Sample code
+  // const {addUser, getAllUsers} = require('../services/users.js');
+  // // const {User} = require('../models/user.js');
+  // getAllUsers().then((res)=>console.log('res:', res))
+  // const User={
+  //   email: 'anchal21107@email.com',
+  //   phoneNumber: '1314567',
+  //   displayName: 'abcgak',
+  //   location: 'athis is location',
+  //   specialty: 'codeing',
+  // }
+  // addUser(User).then((res)=>{
+  //   res.code =='SUCCESS' ? console.log('user added successfully') : console.log(res.message);
+  // });
+  
+  ///sample code
+  // const {addUser, getAllUsers, checkUserExists} = require('../services/users.js');
+  
+  // getAllUsers().then((res)=>console.log('registraton page:', res))
+  // var User = {
+  //   email: '',
+  //   phoneNumber: '',
+  //   displayName: '',
+  //   location: '',
+  //   specialty: '',
+  //   isEmailVerified: false,
+  //   isPhoneVerified: false,
+  // };
+  //   User.email = 'anch11al'
+  //   User.phoneNumber = '12311456789'
+  //   User.displayName = 'abc'
+  //   User.location = 'this is location'
+  //   User.specialty = 'codeing'
+  // checkUserExists(User.email, User.phoneNumber).then((res)=>{
+  //   if (res.code !== 'NOT_FOUND') {
+  //     console.log('restration page: ',res);
+  //   }else{
+  //     addUser(User).then((res)=>{
+  //       res.code =='SUCCESS' ? console.log('user added successfully') : console.log(res.message);
+  //     });
+  //   }
+  // });
+  
---
+++ src/utils/firebase/config.js
@@ -0,0 +1,38 @@
+const firebaseConfig_skipp = {
+    apiKey: "AIzaSyCsp5nhCN16a5OUFn0t-7newhGqQxmsEkA",
+    authDomain: "reflectce-dev.firebaseapp.com",
+    projectId: "reflectce-dev",
+    storageBucket: "reflectce-dev.appspot.com",
+    messagingSenderId: "779269399586",
+    appId: "1:779269399586:web:01a4e2c301e750f339167b",
+    vapidKey: "",
+  };
+  const firebaseConfig_anchal2107 = {
+    apiKey: "AIzaSyACtgdpYXmNDG47N7zF_TOcKtOHkGbp6aM",
+    authDomain: "skipp-reflectce-dev.firebaseapp.com",
+    projectId: "skipp-reflectce-dev",
+    storageBucket: "skipp-reflectce-dev.appspot.com",
+    messagingSenderId: "793470629198",
+    appId: "1:793470629198:web:6daa52482b22f5c2a31a1f",
+    vapidKey:
+      "BFzVOZfWqttLGrET-B88K9YyJPtlksB8LWz9jqXKmKjl88UnfPomtpDjWJfQ1zF3KdfLvY9q9vGXyRSxyqedeTY",
+    serverKey:
+      "AAAAuL6I9U4:APA91bGhXPBVD7OrooRDy7pY9ahCM6Vxqe997BKDzw0YxRrmVicVvYMzb-760ZGVnSv8NPEgSQCuQZ-cVUgauVmt_35sd5uxFgchLTf683gFd9iW0G3tEXr0MzdSPhc00MvwZmKFxyrZ",
+  };
+  
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyACtgdpYXmNDG47N7zF_TOcKtOHkGbp6aM",
+    authDomain: "skipp-reflectce-dev.firebaseapp.com",
+    projectId: "skipp-reflectce-dev",
+    storageBucket: "skipp-reflectce-dev.appspot.com",
+    messagingSenderId: "793470629198",
+    appId: "1:793470629198:web:6daa52482b22f5c2a31a1f",
+    vapidKey:
+      "BFzVOZfWqttLGrET-B88K9YyJPtlksB8LWz9jqXKmKjl88UnfPomtpDjWJfQ1zF3KdfLvY9q9vGXyRSxyqedeTY",
+    serverKey:
+      "AAAAuL6I9U4:APA91bGhXPBVD7OrooRDy7pY9ahCM6Vxqe997BKDzw0YxRrmVicVvYMzb-760ZGVnSv8NPEgSQCuQZ-cVUgauVmt_35sd5uxFgchLTf683gFd9iW0G3tEXr0MzdSPhc00MvwZmKFxyrZ",
+  };
+  
+  module.exports = { firebaseConfig };
+  
